187bcc32fa4eda1df92637939b26f1c420c89328
KouOsada <kouosada.0930@gmail.com>
2021-08-26 11:41:33 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0c1e3923e6305d26340b8f0deb7840c4ef8cf30323a1ec57601a5389df45a55f2e9bdfdfcda3504e870805522886aa6493feefb30162af8966b5ec93f6cefd20'
