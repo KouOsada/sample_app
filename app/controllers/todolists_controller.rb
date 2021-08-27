4579e0edbf387176af035708d6c1671d57824279
KouOsada <kouosada.0930@gmail.com>
2021-08-27 06:43:57 +0000

    [Finish]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -27,6 +27,15 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
